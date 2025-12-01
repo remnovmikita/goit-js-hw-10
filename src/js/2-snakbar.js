80d653d5f99ceea558ce9307b9272ad7bc8c1061
Nikita <remnovmikita@gmail.com>
2025-12-01 19:31:10 +0200

    Second  task

---
+++ src/js/2-snakbar.js
@@ -0,0 +1,43 @@
+const form = document.querySelector('.form');
+const btn = document.querySelector('.form button');
+const input = document.querySelector('[name="delay"]');
+
+
+
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+const userValue = (x) =>{
+    return new Promise((resolve, reject) =>{
+        console.log(`${x}`);
+        const time = Number(input.value);
+        const nameElement = form.elements.state.value;
+        setTimeout(() =>{
+            
+            if (nameElement === "fulfilled" ){
+                resolve("lalalalal");
+            } else{
+                reject("ahahahaha");
+            }
+            console.log(`Таймер сработал через ${time / 1000}s`);
+        }, time);  
+    });
+}
+
+form.addEventListener("submit", event =>{
+    event.preventDefault(); 
+        const time = Number(input.value);
+      userValue("Запуск промиса")
+          .then( x => iziToast.success({
+            position: 'topCenter',
+            message: `✅ Fulfilled promise in ${time}ms`,
+
+            }))   
+          
+            .catch(error => iziToast.error({
+            message: `❌ Rejected promise in ${time}ms`,
+            position: 'topCenter',
+    }));
+});
